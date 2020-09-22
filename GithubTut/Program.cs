baffe76916a03731a5a4176cfe575e7c26712307
Kovács Bence <kovacs.bence.kova@gmail.com>
2020-09-22 10:20:08 +0200

    My code

---
+++ GithubTut/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubTut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("This is a test code for Github upload.");
+            Console.ReadKey();
+        }
+    }
+}
